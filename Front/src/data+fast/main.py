de4df926fc7ea6d05ddaaaead18100b26ca53bd4
LAPTOP-A0RK5QJR\Playdata <sy.lee.991028@gmail.com>
2023-10-17 09:34:41 +0900

    fast api 최종

--- Front/src/data+fast/main.py
+++ Front/src/data+fast/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+import json
+import pandas as pd
 import mysql.connector
 import math
-# from pydantic import BaseModel
+import ast
+import geopy.distance
 
 app = FastAPI()
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # MySQL 연결 설정
 db = mysql.connector.connect(
@@ -18,60 +31,136 @@ cursor = db.cursor()
 # 데이터 조회
 cursor.execute("SELECT sightid, name, type, address1, address2, rating, ratingscore, reviewscore, pricescore, firstimage, latitude, longitude FROM plansight")
 # cursor.execute("SELECT * FROM plansight")
-results = cursor.fetchall()
+sights = cursor.fetchall()
 
-# 가격,별점,리뷰 순
-priceweight = 0.2
-ratingweight = 0.3
-reviewweight = 0.5
-user_weight = [priceweight,ratingweight,reviewweight]
+cursor.execute("SELECT sightid, name, type, address1, address2, rating, ratingscore, reviewscore, pricescore, firstimage, latitude, longitude FROM plansight")
+plan_sights = cursor.fetchall()
 
-# CORS 설정
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+cursor.execute("SELECT hostelid, name, type, address1, address2, tel, homepage, checktime, petinfo, placeinfo, rating, review, theme, themenum, firstimage, latitude, longitude FROM planhostel")
+hostels = cursor.fetchall()
 
+cursor.execute("SELECT schedule_id, title, startdate, enddate, days, accommodation, recommendyn, priceweight, ratingweight, reviewweight, priceprior, ratingprior, reviewprior, sights, transportation, member_userid FROM schedule")
+schedules = cursor.fetchall()
 
-
-# 총점 가중 계산
-weighted_results = []
-for row in results:
-    sightid, name, type, address1, address2, rating, ratingscore, reviewscore, pricescore, firstimage, latitude, longitude = row
-    weighted_score = (ratingscore + reviewscore + pricescore) / 3
-    weighted_results.append((sightid, weighted_score, name, type, address1, address2, rating, ratingscore, reviewscore, pricescore, firstimage, latitude, longitude))
-
-# 가중 점수가 높은 관광지 찾기
-weighted_results.sort(key=lambda x: x[1], reverse=True)
-top_destination = weighted_results[0]
+class WeightData(BaseModel):
+    priceweight: float
+    ratingweight: float
+    reviewweight: float
+    pagenum: int
 
 # 유클리디안 거리 계산 함수
 def euclidean_distance(a, b):
     return math.sqrt(sum((x - y) ** 2 for x, y in zip(a, b)))
 
-# ratingscore, reviewscore, pricescore 기준으로 유클리디안 거리 계산
-target_scores = (top_destination[6], top_destination[7], top_destination[8])  # top_destination의 점수
+@app.post("/recommendations")
+def get_recommendations(weightparameter:WeightData):
+    print(weightparameter)
+    # 가중치 계산
+    dict(weightparameter)
+    priceweight = weightparameter.priceweight
+    ratingweight = weightparameter.ratingweight
+    reviewweight = weightparameter.reviewweight
+    totalweight = priceweight+ratingweight+reviewweight
+    
+    if totalweight != 0:    
+        priceweight /= totalweight
+        ratingweight /= totalweight
+        reviewweight /= totalweight
 
-similar_destinations = []
-for destination in weighted_results:
-    destination_scores = (destination[6], destination[7], destination[8])  # 다른 관광지의 점수
-    distance = euclidean_distance(target_scores, destination_scores)
-    similar_destinations.append((destination, distance))
+    # 총점 가중 계산
+    weighted_sights = []
+    for row in sights:
+        sightid, name, type, address1, address2, rating, ratingscore, reviewscore, pricescore, firstimage, latitude, longitude = row
+        weighted_score = priceweight * (1 - pricescore) + ratingweight * ratingscore + reviewweight * reviewscore
+        weighted_sights.append((sightid, weighted_score, name, type, address1, address2, rating, ratingscore, reviewscore, pricescore, firstimage, latitude, longitude))
 
-# 거리를 기준으로 정렬
-similar_destinations.sort(key=lambda x: x[1])
+    # 가중 점수가 높은 관광지 찾기
+    weighted_sights.sort(key=lambda x: x[1], reverse=True)
+    top_destination = weighted_sights[0]
 
-# 상위 40개 유사한 관광지 선택
-top_similar_destinations = similar_destinations[:40]
+    # ratingscore, reviewscore, pricescore 기준으로 유클리디안 거리 계산
+    target_scores = (top_destination[6], top_destination[7], top_destination[8])  # top_destination의 점수
 
-@app.get("/recommendations")
-def get_recommendations():
-    return top_similar_destinations
-    # return weighted_results
+    similar_destinations = []
+    for destination in weighted_sights:
+        destination_scores = (destination[6], destination[7], destination[8])  # 다른 관광지의 점수
+        distance = euclidean_distance(target_scores, destination_scores)
+        similar_destinations.append((destination, distance))
 
-# if __name__ == "__main__":
-#     import uvicorn
-#     uvicorn.run(app, host="0.0.0.0", port=8000)
+    # 거리를 기준으로 정렬
+    similar_destinations.sort(key=lambda x: x[1])
+
+    # 상위 50개 유사한 관광지 선택
+    top_similar_destinations = similar_destinations[:50]
+    len_similar_destinations=len(top_similar_destinations)
+
+    start = weightparameter.pagenum * 10
+    end = start + 10
+    
+    return {'destinations':top_similar_destinations[start:end], 'total_num':len_similar_destinations}
+
+@app.get("/recommendschedule")
+def get_recommendschedule():
+    schedule_sights = []
+    for row in plan_sights:
+        sightid, name, type, address1, address2, rating, ratingscore, reviewscore, pricescore, firstimage, latitude, longitude = row
+        schedule_sights.append((sightid, name, type, address1, address2, rating, ratingscore, reviewscore, pricescore, firstimage, latitude, longitude))
+
+    schedule_hostels =[]
+    for row in hostels:
+        hostelid, name, type, address1, address2, tel, homepage, checktime, petinfo, placeinfo, rating, review, theme, themenum, firstimage, latitude, longitude = row
+        schedule_hostels.append((hostelid, name, type, address1, address2, tel, homepage, checktime, petinfo, placeinfo, rating, review, theme, themenum, firstimage, latitude, longitude))
+
+    schedule_lists = []
+    for row in schedules:
+        schedule_id, title, startdate, enddate, days, accommodation, recommendyn, priceweight, ratingweight, reviewweight, priceprior, ratingprior, reviewprior, sights, transportation, member_userid = row
+        schedule_lists.append((schedule_id, title, startdate, enddate, days, accommodation, recommendyn, priceweight, ratingweight, reviewweight, priceprior, ratingprior, reviewprior, sights, transportation, member_userid))
+    
+    schedule_sight = pd.DataFrame.from_dict(schedule_sights)
+    schedule_hostel = pd.DataFrame.from_dict(schedule_hostels)
+    schedule_list = pd.DataFrame.from_dict(schedule_lists)
+
+    user_schedule = schedule_list.iloc[-1]
+
+    hostel_names = ast.literal_eval(user_schedule[5])
+    select_hostel = schedule_hostel[schedule_hostel[1].isin(hostel_names)]
+    sight_names = ast.literal_eval(user_schedule[13])
+    select_sights = schedule_sight[schedule_sight[1].isin(sight_names)]
+    
+    jeju_airport = (33.5070537, 126.492776)
+
+    start_locations = []
+    start_locations.append(jeju_airport)
+    for index, row in select_hostel.iterrows():
+        start_location = (row[15], row[16])
+        start_locations.append(start_location)
+    sight_coordinates = [(row[10], row[11], row[1]) for _, row in select_sights.iterrows()]
+
+    start_names = ['Jeju Airport'] + list(select_hostel[1])
+    result_df = pd.DataFrame(columns=['day', 'start_name', 'start_latitude', 'start_longitude', 'sight_name', 'sight_latitude', 'sight_longitude'])
+
+    allocated_sights_per_day = set()  
+
+    for i in range(len(start_locations)):
+        start_location = start_locations[i]
+        start_name = start_names[i]  # 시작 위치의 이름
+    
+        # 시작 위치에서 모든 관광지까지의 거리 계산
+        distances = [(sight[0], sight[1], sight[2], geopy.distance.great_circle(start_location, (sight[0], sight[1])).kilometers) for sight in sight_coordinates]
+
+        # 거리를 기준으로 관광지를 가장 가까운 순으로 정렬
+        distances.sort(key=lambda x: x[3])
+
+        # 가장 가까운 4개의 관광지 할당 (중복 제외)
+        allocated_count = 0
+        for dist in distances:
+            if dist[2] not in allocated_sights_per_day:
+                result_df = result_df.append({'day': i + 1, 'start_name': start_name, 'start_latitude': start_location[0], 'start_longitude': start_location[1], 'sight_name': dist[2], 'sight_latitude': dist[0], 'sight_longitude': dist[1]}, ignore_index=True)
+                allocated_sights_per_day.add(dist[2])
+                allocated_count += 1
+                if allocated_count >= 4:
+                    break
+
+    # # dataframe을 json으로 변환
+    schedule_result = result_df.to_json(orient="records")
+    return json.loads(schedule_result)
--- Front/src/pages/createplan/recommendYes/ChoiceSightY.js
+++ Front/src/pages/createplan/recommendYes/ChoiceSightY.js
@@ -1,36 +1,75 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+// import React, { useState, useEffect } from "react";
 
 function ChoiceSight() {
-    const [sights, setSights] = useState([]);
+    // const [sights, setSights] = useState([]);
     const [selectedSights, setSelectedSights] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);   // 현재 페이지
+    const [pageSize] = useState(15);   // 페이지 크기
+    const [totalPages, setTotalPages] = useState(0);   // 전체 페이지 수
     const navigate = useNavigate();
 
-    // 선택한 날짜, 숙소 정보 읽어오기
+    // 선택한 날짜, 숙소, 추천여부, 가중치 정보 읽어오기
     const location = useLocation();
-    const {selectedStartDate, selectedEndDate, selectedHostels, selectedRecommedYn} = location.state;
+    const {selectedStartDate, selectedEndDate, selectedHostels, selectedRecommedYn, inputPriceWeight, inputRatingWeight, inputReviewWeight} = location.state;
     // const selectedStartDate = location.state.startDate;
     // const selectedEndDate = location.state.endDate;
     // const selectedHostels = location.state.selectedHostels;
     console.log(selectedHostels)
-    console.log(selectedRecommedYn)
-    
-    // 관광지 목록 호출
-    useEffect(() => {
-        axios.get("http://localhost:8080/createplan/choicesights")
-        .then((response) => {
-            console.log(response.data);
-            setSights(response.data);
-        })
-        .catch((error) => {
-            console.error('Error fetching data :', error)
-        });
-    }, []);
+    console.log('추천여부:', selectedRecommedYn)
+    console.log('가격 가중치: ', inputPriceWeight)
 
-    // 다음 페이지 이동 및 선택한 숙소&관광지 정보 전달
+    const [sightList, setSightList] = useState([]);
+
+    // weightdata = {}
+    // weightdata = {priceweight:parseFloat(inputPriceWeight),ratingweight:parseFloat(inputRatingWeight),reviewweight:parseFloat(inputReviewWeight)}
+    // 관광지 목록 호출
+    // setCurrentPage(1)
+    // console.log({priceweight:parseFloat(inputPriceWeight),ratingweight:parseFloat(inputRatingWeight),reviewweight:parseFloat(inputReviewWeight)})
+    useEffect(() => {
+        // fast api서버에서 페이징된 숙소 목록을 가져오는 요청
+        axios.post("http://localhost:8000/recommendations",{priceweight:parseFloat(inputPriceWeight),ratingweight:parseFloat(inputRatingWeight),reviewweight:parseFloat(inputReviewWeight), pagenum:currentPage})
+            .then((response) => {  
+                const data = response.data.destinations
+                setSightList(data)
+                setTotalPages(response.data.total_num)
+            })
+            .catch((error) => {
+                console.error("데이터 불러오기 오류:", error);
+            });
+        }, [currentPage,pageSize]);
+
+    // 페이지 번호 목록 생성
+    const pageNumbers = [];
+    let total_num = Math.ceil(totalPages/10)
+    for (let i = 0 ; i <= total_num; i++) {
+        pageNumbers.push(i);
+    }
+
+    // 페이지 번호 이전으로 이동
+    const moveToPreviousPage = () => {
+        if (currentPage > 0) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    // 페이지 번호 다음으로 이동
+    const moveToNextPage = () => {
+        if (currentPage < totalPages - 1) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+    // 페이지 변경 함수
+    const handlePageChange = (newPage) => {
+        setCurrentPage(newPage);
+    }
+
+    // 다음 페이지 이동 및 선택한 날짜, 숙소, 추천여부, 가중치, 관광지 정보 전달
     const moveNextClick = () => {
-        navigate('/createplan/y/choicetransportation', {state: {selectedStartDate, selectedEndDate, selectedHostels, selectedRecommedYn, selectedSights}})
+        navigate('/createplan/y/choicetransportation', {state: {selectedStartDate, selectedEndDate, selectedHostels, selectedRecommedYn : 'Y', inputPriceWeight, inputRatingWeight, inputReviewWeight, selectedSights}})
     }
 
     // 선택 버튼 클릭 시 호출되는 함수
@@ -38,26 +77,39 @@ function ChoiceSight() {
         setSelectedSights([...selectedSights, sight]);
     }
 
-    console.log(selectedSights)
-
     return (
-        <div>
+        <div className="container my-3">
             <h2><b>관광지 선택</b></h2>
             <br/>
             <div>
-                {sights.map((sight) => {
-                    return (
-                        <ul>
-                            <li key={sight.sight_id}>
-                                    <h4>Name: {sight.name}</h4>
-                                    <p>Type: {sight.type}</p>
-                                    <p>Address: {sight.address1}</p>
-                                    <p>⭐: {sight.rating}</p>
-                                    <button className='btn btn-outline-secondary' onClick={() => handleSightSelect(sight)}>선택</button>
-                            </li>
-                        </ul>
-                    )
-                })}
+                {sightList.map((sight,index) => {
+                        return (
+                            <div key={index} style={{display:'flex', alignItems:'center'}}>
+                                <div>
+                                    <h5><b>Name: {sight[0][2]}</b></h5>
+                                    <p>Type: {sight[0][3]}</p>
+                                    <p>Address: {sight[0][4] == '없음' ? sight[0][5] : sight[0][4]}</p>
+                                    <p>⭐ {sight[0][6]}</p>
+                                </div>
+                                <button style={{borderRadius:'5px', borderColor:'lightgray', marginLeft:'10px'}} className='btn btn-outline-secondary' onClick={() => handleSightSelect(sight)}>+</button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+            <div className="pagination justify-content-center">
+                <ul className="pagination">
+                    <li className={`page-item ${currentPage === 0 ? "disabled" : ""}`}>
+                        <button className="page-link" onClick={moveToPreviousPage} disabled={currentPage === 0}>이전</button>
+                    </li>
+                    {pageNumbers.map((page) => (<li key={page} className={`page-item ${page === currentPage ? "active" : ""}`}>
+                        <button className="page-link" onClick={() => handlePageChange(page)}>{page + 1}</button>
+                    </li>
+                    ))}
+                    <li className={`page-item ${currentPage === totalPages - 1 ? "disabled" : ""}`}>
+                        <button className="page-link" onClick={moveToNextPage} disabled={currentPage === totalPages - 1}>다음</button>
+                    </li>
+                </ul>
             </div>
             <hr/>
             <div style={{display:'flex', justifyContent:'flex-end'}}>
